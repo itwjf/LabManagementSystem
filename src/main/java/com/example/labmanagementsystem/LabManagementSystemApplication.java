6de63614bbdbb8574412ebd447ea997e25b22250
itwjf <2933277628@qq.com>
2025-12-23 22:36:43 +0800

    feat: working docker mysql + spring boot config

---
+++ src/main/java/com/example/labmanagementsystem/LabManagementSystemApplication.java
@@ -0,0 +1,14 @@
+package com.example.labmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabManagementSystemApplication {
+
+	public static void main(String[] args) {SpringApplication.run(LabManagementSystemApplication.class, args);
+
+	
+	}
+
+}
